7079d8b45d644afcce2b0c8ac8aa183973a30a46
sandeep066 <12527032+sandeep066@users.noreply.github.com>
2024-02-06 17:42:47 +0530

    chore:wireframe css html code for web instagram

--- src/assets/icons/HeartIcon.tsx
+++ src/assets/icons/HeartIcon.tsx
@@ -1,17 +1,41 @@
-// HeartIcon.tsx
-import React from "react";
+import React, { useState } from "react";
 
-const HeartIcon: React.FC = () => (
-  <svg
-    xmlns="http://www.w3.org/2000/svg"
-    width="24"
-    height="24"
-    fill="currentColor"
-    className="bi bi-heart"
-    viewBox="0 0 16 16"
-  >
-    <path d="m8 2.748-.717-.737C5.6.281 2.514.878 1.4 3.053c-.523 1.023-.641 2.5.314 4.385.92 1.815 2.834 3.989 6.286 6.357 3.452-2.368 5.365-4.542 6.286-6.357.955-1.886.838-3.362.314-4.385C13.486.878 10.4.28 8.717 2.01zM8 15C-7.333 4.868 3.279-3.04 7.824 1.143q.09.083.176.171a3 3 0 0 1 .176-.17C12.72-3.042 23.333 4.867 8 15" />
-  </svg>
-);
+const HeartIcon = () => {
+  const [isLiked, setIsLiked] = useState(false);
+
+  const handleLike = () => {
+    setIsLiked((prevIsLiked) => !prevIsLiked);
+    // Additional logic for updating likes count on the backend
+  };
+
+  return (
+    <span className="heart-icon" onClick={handleLike}>
+      {isLiked ? (
+        <svg
+          xmlns="http://www.w3.org/2000/svg"
+          id="Layer_1"
+          data-name="Layer 1"
+          viewBox="0 0 24 24"
+          width="20"
+          height="20"
+          fill="#e51010"
+        >
+          <path d="M17.5.917a6.4,6.4,0,0,0-5.5,3.3A6.4,6.4,0,0,0,6.5.917,6.8,6.8,0,0,0,0,7.967c0,6.775,10.956,14.6,11.422,14.932l.578.409.578-.409C13.044,22.569,24,14.742,24,7.967A6.8,6.8,0,0,0,17.5.917Z" />
+        </svg>
+      ) : (
+        <svg
+          xmlns="http://www.w3.org/2000/svg"
+          viewBox="0 0 24 24"
+          width="20"
+          height="20"
+        >
+          <g id="_01_align_center" data-name="01 align center">
+            <path d="M17.5.917a6.4,6.4,0,0,0-5.5,3.3A6.4,6.4,0,0,0,6.5.917,6.8,6.8,0,0,0,0,7.967c0,6.775,10.956,14.6,11.422,14.932l.578.409.578-.409C13.044,22.569,24,14.742,24,7.967A6.8,6.8,0,0,0,17.5.917ZM12,20.846c-3.253-2.43-10-8.4-10-12.879a4.8,4.8,0,0,1,4.5-5.05A4.8,4.8,0,0,1,11,7.967h2a4.8,4.8,0,0,1,4.5-5.05A4.8,4.8,0,0,1,22,7.967C22,12.448,15.253,18.416,12,20.846Z" />
+          </g>
+        </svg>
+      )}
+    </span>
+  );
+};
 
 export default HeartIcon;
